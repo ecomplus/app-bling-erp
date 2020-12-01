a69f7ee22e35a482f48debc98c4fff078d138d7f
Leonardo Matos <leomp120894@gmail.com>
2020-12-01 14:47:58 -0300

    fix(export-product): properly linking product variations after main product

--- functions/lib/integration/export-product.js
+++ functions/lib/integration/export-product.js
@@ -60,47 +60,43 @@ module.exports = ({ appSdk, storeId }, blingToken, blingStore, queueEntry, appDa
         })
 
         .then(response => {
-          console.log(response.data)
-          if (response) {
-            console.log(123)
-            if (blingStore && (canCreateNew || appData.export_price)) {
-              const blingProducts = response.data && response.data.produtos
-              if (Array.isArray(blingProducts) && blingProducts.length) {
-                const promises = blingProducts.map(({ produto }) => {
-                  if (!produto) {
-                    return null
-                  }
-                  const method = produto.produtoLoja ? 'put' : 'post'
-                  const data = {
-                    produtosLoja: {
-                      produtoLoja: {
-                        idLojaVirtual: product._id
-                      }
-                    }
-                  }
-                  data.produtosLoja.produtoLoja.preco = ecomUtils.onPromotion(product)
-                    ? {
-                        preco: product.base_price,
-                        precoPromocional: ecomUtils.price(product)
-                      }
-                    : {
-                        preco: ecomUtils.price(product)
-                      }
-                  const endpoint = `/produtoLoja/${blingStore}/${produto.codigo}`
-                  const promise = bling[method](endpoint, data)
-                  if (method === 'put') {
-                    promise.catch(err => {
-                      if (err.response && err.response.status === 404) {
-                        return bling.post(endpoint, data)
-                      }
-                      throw err
-                    })
-                  }
-                  return promise
-                })
-                return Promise.all(promises).then(([response]) => response)
+          if (blingStore && (canCreateNew || appData.export_price)) {
+            const promises = []
+            const linkBlingProduct = (codigo, idLojaVirtual) => {
+              const method = originalBlingProduct ? 'put' : 'post'
+              const data = {
+                produtosLoja: {
+                  produtoLoja: { idLojaVirtual }
+                }
               }
+              data.produtosLoja.produtoLoja.preco = ecomUtils.onPromotion(product)
+                ? {
+                    preco: product.base_price,
+                    precoPromocional: ecomUtils.price(product)
+                  }
+                : {
+                    preco: ecomUtils.price(product)
+                  }
+              const endpoint = `/produtoLoja/${blingStore}/${codigo}`
+              const promise = bling[method](endpoint, data)
+              if (method === 'put') {
+                promise.catch(err => {
+                  if (err.response && err.response.status === 404) {
+                    return bling.post(endpoint, data)
+                  }
+                  throw err
+                })
+              }
+              promises.push(promise)
             }
+
+            linkBlingProduct(blingProductCode, product._id)
+            if (product.variations) {
+              product.variations.forEach(({ _id, sku }, i) => {
+                linkBlingProduct(sku || `${blingProductCode}-${(i + 1)}`, `${product._id}/${_id}`)
+              })
+            }
+            return Promise.all(promises).then(([response]) => response)
           }
           return response
         })
