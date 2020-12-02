46e5455e61eff5dae63ba715d064d8182bf72251
Leonardo Matos <leomp120894@gmail.com>
2020-12-02 12:36:20 -0300

    chore(bling-callback): try req.body.data (string) when not body.retorno not set

--- functions/routes/bling/callback.js
+++ functions/routes/bling/callback.js
@@ -1,14 +1,22 @@
-const Bling = require('../../lib/bling/constructor')
+// const Bling = require('../../lib/bling/constructor')
 
 exports.post = ({ appSdk, admin }, req, res) => {
   const blingToken = req.query.token
   const storeId = parseInt(req.query.store_id, 10)
 
   if (storeId > 100 && typeof blingToken === 'string' && blingToken && req.body) {
-    console.log(req.body)
-    console.log(req.params)
-    const bling = new Bling(blingToken)
-    return res.sendStatus(200)
+    let { retorno } = req.body
+    if (!retorno && typeof req.body.data === 'string') {
+      try {
+        retorno = JSON.parse(req.body.data)
+      } catch (e) {
+      }
+    }
+    if (retorno) {
+      console.log(retorno)
+      // const bling = new Bling(blingToken)
+      return res.sendStatus(200)
+    }
   }
 
   res.sendStatus(403)
