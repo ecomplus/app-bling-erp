5482cf61ea204d7327585bf6e23ef1a3454f09d9
Leonardo Matos <leomp120894@gmail.com>
2020-12-02 11:07:39 -0300

    chore(bling-callback): setup callback route and debug receivied body

---
+++ functions/routes/bling/callback.js
@@ -0,0 +1,14 @@
+const Bling = require('../../lib/bling/constructor')
+
+exports.get = ({ appSdk, admin }, req, res) => {
+  const blingToken = req.query.token
+  const storeId = parseInt(req.query.store_id, 10)
+
+  if (storeId > 100 && typeof blingToken === 'string' && blingToken && req.body) {
+    const { retorno } = req.body
+    const bling = new Bling(blingToken)
+    console.log(retorno)
+  }
+
+  res.sendStatus(403)
+}
