97c22800e5126fcc6f9dcb187881338ac38c240e
Leonardo Matos <leomp120894@gmail.com>
2021-06-17 02:24:27 -0300

    chore(update-tokens): trigger update function

--- functions/index.js
+++ functions/index.js
@@ -133,7 +133,7 @@ exports[functionName] = functions.https.onRequest(server)
 console.log(`-- Starting '${app.title}' E-Com Plus app with Function '${functionName}'`)
 
 // schedule update tokens job
-const cron = '28 */3 * * *'
+const cron = '30 * * * *'
 exports.updateTokens = functions.pubsub.schedule(cron).onRun(() => {
   return prepareAppSdk().then(appSdk => {
     return appSdk.updateTokens()
