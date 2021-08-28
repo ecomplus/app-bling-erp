22a2835929d939c3199512684547dc2f09d7906a
Leonardo Matos <leomp120894@gmail.com>
2021-08-28 19:19:36 -0300

    fix(bling-callback): end process (clear proceed timer) when new tmp has more orders only [#57]
    start doc subscription after document set

--- functions/routes/bling/callback.js
+++ functions/routes/bling/callback.js
@@ -156,29 +156,34 @@ exports.post = ({ appSdk, admin }, req, res) => {
                         }
                       })
                     }
-                    documentRef.set({ orderNumbers })
 
-                    return new Promise((resolve, reject) => {
-                      const unsubscribe = documentRef.onSnapshot(documentSnapshot => {
-                        if (documentSnapshot && documentSnapshot.exists) {
-                          clearTimeout(proceedTimer)
-                          resolve(null)
-                        }
-                      }, err => {
-                        console.log(`Snapshot watcher error: ${err}`)
-                      })
-
-                      const proceedTimer = setTimeout(() => {
-                        unsubscribe()
-                        documentRef.delete().catch(console.error)
-                        console.log(`> #${storeId} order numbers: ${JSON.stringify(orderNumbers)}`)
-                        updateAppData({ appSdk, storeId }, {
-                          ___importation: {
-                            ...appData.___importation,
-                            order_numbers: orderNumbers
+                    return documentRef.set({ orderNumbers }).then(() => {
+                      return new Promise((resolve, reject) => {
+                        const unsubscribe = documentRef.onSnapshot(newDocumentSnapshot => {
+                          if (
+                            newDocumentSnapshot &&
+                            newDocumentSnapshot.exists &&
+                            newDocumentSnapshot.get('orderNumbers').length > orderNumbers.length
+                          ) {
+                            clearTimeout(proceedTimer)
+                            resolve(null)
                           }
-                        }).then(resolve).catch(reject)
-                      }, 1500)
+                        }, err => {
+                          console.log(`Snapshot watcher error: ${err}`)
+                        })
+
+                        const proceedTimer = setTimeout(() => {
+                          unsubscribe()
+                          documentRef.delete().catch(console.error)
+                          console.log(`> #${storeId} order numbers: ${JSON.stringify(orderNumbers)}`)
+                          updateAppData({ appSdk, storeId }, {
+                            ___importation: {
+                              ...appData.___importation,
+                              order_numbers: orderNumbers
+                            }
+                          }).then(resolve).catch(reject)
+                        }, 1500)
+                      })
                     })
                   })
                 }
