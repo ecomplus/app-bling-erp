43b0c2659b6344aebb38151db80487cf8e70bc65
Leonardo Matos <leomp120894@gmail.com>
2021-10-11 19:23:08 -0300

    perf(clear-order-states): also clear old integrations retries entries

--- functions/lib/integration/clear-order-states.js
+++ functions/lib/integration/clear-order-states.js
@@ -4,14 +4,25 @@ module.exports = context => {
   const date = new Date()
   date.setDate(date.getDate() - 1)
 
-  return firestore()
-    .collection('bling_orders')
-    .where('updatedAt', '<', firestore.Timestamp.fromDate(date))
-    .limit(500)
-    .get().then(async querySnapshot => {
-      for (const doc of querySnapshot.docs) {
-        await doc.ref.delete()
-      }
+  const delOldDocs = (collName, fieldName, fieldVal) => {
+    return firestore()
+      .collection(collName)
+      .where(fieldName, '<', fieldVal)
+      .limit(500)
+      .get().then(async querySnapshot => {
+        for (const doc of querySnapshot.docs) {
+          await doc.ref.delete()
+        }
+        return querySnapshot
+      })
+  }
+
+  return delOldDocs('bling_orders', 'updatedAt', firestore.Timestamp.fromDate(date))
+    .then(querySnapshot => {
       console.log(`> Deleted ${querySnapshot.size} Bling order states`)
+      return delOldDocs('integration_retries', 'd', date.toISOString())
+        .then(querySnapshot => {
+          console.log(`> Deleted ${querySnapshot.size} integration retries`)
+        })
     })
 }
