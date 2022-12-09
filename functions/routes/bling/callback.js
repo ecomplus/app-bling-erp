e8c2d5a86739e2dc23b2576c02e06443b9a40fd1
Wisley Alves <wisley7lagoas@yahoo.com.br>
2022-12-09 10:24:51 -0300

    fix(pub-sub): add pubsub in webhook)

--- functions/routes/bling/callback.js
+++ functions/routes/bling/callback.js
@@ -1,17 +1,22 @@
-const axios = require('axios')
-const { baseUri } = require('./../../__env')
-const getAppData = require('./../../lib/store-api/get-app-data')
-const updateAppData = require('./../../lib/store-api/update-app-data')
-const importProduct = require('./../../lib/integration/import-product')
+
+const PubSub = require('@google-cloud/pubsub').PubSub
+const getPubSubTopic = require('../../lib/pubsub/create-topic').getPubSubTopic
+
+const sendMessageTopic = async (eventName, json) => {
+  const topicName = getPubSubTopic(eventName)
+  const messageId = await new PubSub()
+    .topic(topicName)
+    .publishMessage({ json })
+
+  console.log('>> MessageId: ', messageId, ' Topic: ', topicName)
+
+  return Promise.resolve(200)
+}
 
 exports.post = ({ appSdk, admin }, req, res) => {
-  const startTime = Date.now()
+  // const startTime = Date.now()
   const blingToken = req.query.token
   const storeId = parseInt(req.query.store_id, 10)
-  let retries = parseInt(req.query.retries, 10)
-  if (isNaN(retries) || retries < 0) {
-    retries = 0
-  }
 
   if (storeId > 100 && typeof blingToken === 'string' && blingToken && req.body) {
     let { retorno } = req.body
@@ -28,201 +33,23 @@ exports.post = ({ appSdk, admin }, req, res) => {
       TODO: check Bling server IPs
       const clientIp = req.get('x-forwarded-for') || req.connection.remoteAddress
       */
-      res.sendStatus(200)
 
-      return appSdk.getAuth(storeId)
-        .then(auth => {
-          const appClient = { appSdk, storeId, auth }
-          return getAppData(appClient)
-            .then(appData => {
-              if (appData.bling_api_token !== blingToken) {
-                return null
-              }
-              let { estoques, pedidos } = retorno
-              if (Array.isArray(estoques)) {
-                if (Array.isArray(estoques[0])) {
-                  estoques = estoques[0]
-                  for (let i = 1; i < estoques.length; i++) {
-                    estoques = estoques.concat(estoques[i])
-                  }
-                }
-
-                if (estoques.length) {
-                  if (appData.import_quantity === false || appData.export_quantity) {
-                    return null
-                  }
-                  const blingStore = appData.bling_store
-                  const blingDeposit = appData.bling_deposit
-
-                  return new Promise((resolve, reject) => {
-                    const saveToQueue = () => {
-                      let skus = appData.___importation && appData.___importation.skus
-                      if (!Array.isArray(skus)) {
-                        skus = []
-                      }
-                      const promises = []
-                      estoques.forEach(({ estoque }, i) => {
-                        if (estoque && estoque.codigo) {
-                          const sku = String(estoque.codigo)
-                          console.log(`> Bling callback: #${storeId} ${sku}`)
-                          promises.push(new Promise(resolve => {
-                            setTimeout(() => {
-                              admin.firestore().collection('bling_stock_updates').add({
-                                ref: `${storeId}_${blingToken}_${sku}`,
-                                estoque
-                              })
-                                .then(() => {
-                                  if (!skus.includes(sku)) {
-                                    skus.push(sku)
-                                  }
-                                  resolve()
-                                })
-                                .catch(console.error)
-                            }, i * 330)
-                          }))
-                        }
-                      })
-
-                      if (promises.length) {
-                        return Promise.all(promises)
-                          .then(() => {
-                            console.log(`> #${storeId} SKUs: ${JSON.stringify(skus)}`)
-                            return updateAppData({ appSdk, storeId }, {
-                              ___importation: {
-                                ...appData.___importation,
-                                skus
-                              }
-                            })
-                          })
-                          .then(resolve)
-                          .catch(reject)
-                      }
-                      resolve(true)
-                    }
-
-                    let skuIndex = 0
-                    const tryNextSku = () => {
-                      const blingStockUpdate = estoques[skuIndex] && estoques[skuIndex].estoque
-                      const nextId = blingStockUpdate && blingStockUpdate.codigo
-                      if (nextId) {
-                        console.log(`> Bling callback: #${storeId} ${nextId} => ${JSON.stringify(blingStockUpdate)}`)
-                        const queueEntry = {
-                          nextId,
-                          blingStockUpdate,
-                          isNotQueued: true,
-                          cb: (err, isDone) => {
-                            if (!err && isDone) {
-                              estoques.splice(skuIndex, 1)
-                              skuIndex++
-                              return tryNextSku()
-                            }
-                            saveToQueue()
-                          }
-                        }
-                        importProduct(appClient, blingToken, blingStore, blingDeposit, queueEntry, appData, false, true)
-                          .catch(saveToQueue)
-                      } else {
-                        saveToQueue()
-                      }
-                    }
-                    tryNextSku()
-                  })
-                }
-              }
-
-              if (Array.isArray(pedidos)) {
-                let orderNumbers = appData.___importation && appData.___importation.order_numbers
-                if (!Array.isArray(orderNumbers)) {
-                  orderNumbers = []
-                }
-                let hasNewOrder = false
-                pedidos.forEach(({ pedido }) => {
-                  if (pedido && pedido.numero) {
-                    const orderNumber = String(pedido.numero)
-                    if (!orderNumbers.includes(orderNumber)) {
-                      orderNumbers.push(orderNumber)
-                      hasNewOrder = true
-                    }
-                  }
-                })
-
-                if (hasNewOrder) {
-                  const documentRef = admin.firestore().doc(`bling_orders_tmp/${storeId}`)
-                  return documentRef.get().then(documentSnapshot => {
-                    if (documentSnapshot.exists) {
-                      documentSnapshot.get('orderNumbers').forEach(orderNumber => {
-                        if (!orderNumbers.includes(orderNumber)) {
-                          orderNumbers.push(orderNumber)
-                        }
-                      })
-                    }
-
-                    return documentRef.set({ orderNumbers }).then(() => {
-                      return new Promise((resolve, reject) => {
-                        const unsubscribe = documentRef.onSnapshot(newDocumentSnapshot => {
-                          if (
-                            newDocumentSnapshot &&
-                            newDocumentSnapshot.exists &&
-                            newDocumentSnapshot.get('orderNumbers').length > orderNumbers.length
-                          ) {
-                            clearTimeout(proceedTimer)
-                            resolve(null)
-                          }
-                        }, err => {
-                          console.log(`Snapshot watcher error: ${err}`)
-                        })
-
-                        const proceedTimer = setTimeout(() => {
-                          unsubscribe()
-                          documentRef.delete().catch(console.error)
-                          console.log(`> #${storeId} order numbers: ${JSON.stringify(orderNumbers)}`)
-                          updateAppData({ appSdk, storeId }, {
-                            ___importation: {
-                              ...appData.___importation,
-                              order_numbers: orderNumbers
-                            }
-                          }).then(resolve).catch(reject)
-                        }, 1500)
-                      })
-                    })
-                  })
-                }
-              }
-              return null
-            })
+      return sendMessageTopic('bling', { blingToken, storeId, retorno })
+        .then(() => {
+          return res.sendStatus(200)
         })
-
-        .then(payload => {
-          if (!res.headersSent) {
-            res.sendStatus(200)
-          }
-          return payload
-        })
-
         .catch(err => {
           err.storeId = storeId
+          err.blingToken = blingToken
           console.error(err)
-          if (!(retries > 3) && !err.appWithoutAuth) {
-            retries++
-            setTimeout(() => {
-              axios.post(`${baseUri}/bling/callback`, req.body, {
-                params: {
-                  store_id: storeId,
-                  token: blingToken,
-                  retries
-                }
-              }).catch(console.error)
-            }, Math.min(50 * 1000 - (Date.now() - startTime), 40 * 1000))
-          }
-          if (!res.headersSent) {
-            res.sendStatus(502)
-          }
+          return res.sendStatus(502)
         })
     } else {
       console.log(`#${storeId} unexpected Bling callback: ${JSON.stringify(req.body)}`)
       return res.status(200).send('Ignoring invalid request body')
     }
   }
+  console.log('> ', storeId, ' => ', blingToken, ' ', typeof blingToken === 'string')
 
-  res.sendStatus(403)
+  return res.sendStatus(403)
 }
