91f6eda16d65301c2d89b416f882b75092a260f8
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2023-01-23 11:13:36 -0300

    chore: check log for one more store

--- functions/routes/bling/callback.js
+++ functions/routes/bling/callback.js
@@ -29,6 +29,9 @@ exports.post = ({ appSdk, admin }, req, res) => {
     }
 
     if (retorno) {
+      if (Number(storeId) === 51331) {
+        console.log('Callback bling 51331', JSON.stringify(retorno))
+      }
       /*
       TODO: check Bling server IPs
       const clientIp = req.get('x-forwarded-for') || req.connection.remoteAddress
